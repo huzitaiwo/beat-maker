ad12a5f9cc66281bf2246488304a1715be6e817b
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-19 21:10:37 +0100

    added click evtn to the play button

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 class Drumkit {
     constructor() {
         this.pads = document.querySelectorAll('.pad');
+        this.playBtn = document.querySelector('.play');
         this.kickAudio = document.querySelectorAll('.kick-sound');
         this.snareAudio = document.querySelectorAll('.snare-sound');
         this.hihatAudio = document.querySelectorAll('.hihat-sound');
         this.index = 0;
+        this.bpm = 150;
+    }
+    activePad() {
+        this.classList.toggle('active');
     }
     repeat() {
         let step = this.index % 8;
+        this.index++;
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        // console.log(step);
+    }
+    start() {
+    // console.log(this);
+        const interval = (60/this.bpm) * 1000;
+        setInterval(() => this.repeat() , interval);
     }
 }
+
+const drumkit = new Drumkit();
+
+drumkit.pads.forEach(pad => {
+    pad.addEventListener('click', drumkit.activePad);
+})
+
+drumkit.playBtn.addEventListener('click', () => {
+    drumkit.start();
+})
